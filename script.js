f4806f8ad3d8ad3c790e33e28f7267ceaea428d1
Fridah-kalee <fridahkalee@gmail.com>
2022-03-13 18:14:23 +0300

    adding project classes to portfolio

---
+++ script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+
+    $("what1").click(function(){
+        $("hide1").slideToggle();
+        $("show1").slideToggle();
+    });
+
+    $("what2").click(function(){
+        $("hide2").slideToggle();
+        $("show2").slideToggle();
+    });
+
+    $("what3").click(function(){
+        $("hide3").slideToggle();
+        $("show3").slideToggle();
+    });
+
+
+});
