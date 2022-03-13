3674231a71abf82043ffd8ed10098383c47b0102
Fridah-kalee <fridahkalee@gmail.com>
2022-03-13 18:18:58 +0300

    making site more interactive with jQuery

--- script.js
+++ script.js
@@ -15,5 +15,29 @@ $(document).ready(function(){
         $("show3").slideToggle();
     });
 
+    $("#submit").click(function(){
+        var name = document.getElementById("name").value;
+        var email = document.getElementById("email").value;
+        var message = document.getElementById("message").value;
+    
+        if(name == ""){
+          alert("enter your name")
+        } 
+        else if(email == ""){
+          alert("enter your email")
+        }
+        else if(message == ""){
+          alert("write a message")
+        }
+        else {
+         alert(name + " " + "we have received your message. Thank you for reaching out to us.")
+        }
+      })
+    
+      $(".proj").hide()
+      $(".project").hover(function(){
+        $(this).find(".proj").toggle(1000)
+      });
+
 
 });
